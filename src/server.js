b192c8f4d9fb807683007c5120651d20ced96bb1
stevanryan <stevanryan85@gmail.com>
2023-07-07 21:25:41 +0700

    updating codes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./service/inMemory/NotesService');
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -12,7 +14,13 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  // Mendaftarkan plugin di server Hapi.
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
